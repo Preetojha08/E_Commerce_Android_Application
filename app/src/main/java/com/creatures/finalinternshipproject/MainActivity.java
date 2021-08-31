ed3027c6ed7d1a8932215216f5024c4189aecb47
Preet Ojha <ozapreet08@gmail.com>
2021-09-01 00:52:22 +0530

    Login Commit

--- app/src/main/java/com/creatures/finalinternshipproject/MainActivity.java
+++ app/src/main/java/com/creatures/finalinternshipproject/MainActivity.java
@@ -2,7 +2,12 @@ package com.creatures.finalinternshipproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +15,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+        int splash_time = 3000; //This is 3 seconds
+
+        ImageView imageView = findViewById(R.id.logo_image_view);
+        Animation animation = new AlphaAnimation(0,1);
+        animation.setDuration(2500);
+
+        imageView.setAnimation(animation);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(i);
+                finish();
+            }
+        },splash_time);
 
 
 
