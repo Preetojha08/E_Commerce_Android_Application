d5debde13d3abb09c2b74d8bcced5a75f1c75ba1
Preet Ojha <ozapreet08@gmail.com>
2021-09-02 14:09:33 +0530

    Navigation UI Commit

--- app/src/main/java/com/creatures/finalinternshipproject/HomeActivity.java
+++ app/src/main/java/com/creatures/finalinternshipproject/HomeActivity.java
@@ -1,6 +1,10 @@
 package com.creatures.finalinternshipproject;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -8,11 +12,14 @@ import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     RecyclerView recyclerView_cat,recyclerView_product;
 
@@ -25,12 +32,14 @@ public class HomeActivity extends AppCompatActivity {
     LinearLayoutManager HorizontalLayout;
     RecyclerViewAdapter adapter;
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_home_portal);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("E-Commerce App");
 
         recyclerView_cat=(RecyclerView)findViewById(R.id.recyclerView_Categories);
         recyclerView_product=(RecyclerView)findViewById(R.id.recyclerView_product);
@@ -48,7 +57,7 @@ public class HomeActivity extends AppCompatActivity {
         cat_img.add(R.drawable.sports);
         cat_img.add(R.drawable.cloths);
 
-        adapter = new RecyclerViewAdapter(cat_title,cat_img,this);
+        adapter = new RecyclerViewAdapter(cat_title,cat_img,this,10);
 
         /*recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
         recyclerView.setAdapter(adapter);*/
@@ -61,17 +70,17 @@ public class HomeActivity extends AppCompatActivity {
         pro_img = new ArrayList<>();
         pro_title = new ArrayList<>();
 
-        pro_title.add("Headphone");
-        pro_title.add("Iphone 12");
-        pro_title.add("LV T-Shirt");
-        pro_title.add("SG Bat");
+        pro_title.add("Headphone\n250 ₹");
+        pro_title.add("Iphone 12\n90,000 ₹");
+        pro_title.add("LV T-Shirt\n500 ₹");
+        pro_title.add("SG Bat\n100 ₹");
 
         pro_img.add(R.drawable.headphone);
         pro_img.add(R.drawable.mobile);
         pro_img.add(R.drawable.t_shirt);
         pro_img.add(R.drawable.bat);
 
-        adapter = new RecyclerViewAdapter(pro_title,pro_img,this);
+        adapter = new RecyclerViewAdapter(pro_title,pro_img,this,20);
 
         GridLayoutManager layoutManager=new GridLayoutManager(this,2);
 
@@ -79,8 +88,23 @@ public class HomeActivity extends AppCompatActivity {
         recyclerView_product.setAdapter(adapter);
 
 
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        setTitle("NGO System Pro");
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setItemIconTintList(null);
+        navigationView.setNavigationItemSelectedListener(this);
 
 
 
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
     }
 }
--- app/src/main/java/com/creatures/finalinternshipproject/RecyclerViewAdapter.java
+++ app/src/main/java/com/creatures/finalinternshipproject/RecyclerViewAdapter.java
@@ -1,11 +1,13 @@
 package com.creatures.finalinternshipproject;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -18,27 +20,49 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
     List<Integer> images;
     LayoutInflater layoutInflater;
     Context context;
+    int card_val=0;
 
-    public RecyclerViewAdapter(List<String> title, List<Integer> images, Context context) {
+    public RecyclerViewAdapter(List<String> title, List<Integer> images, Context context,int card_val) {
         this.title = title;
         this.images = images;
         this.context = context;
         this.layoutInflater = LayoutInflater.from(context);
+        this.card_val = card_val;
     }
 
     @NonNull
     @Override
     public card_view_holder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view_oncreate;
-        view_oncreate = layoutInflater.inflate(R.layout.card_layout,parent,false);
+
+        if (card_val==10){
+            view_oncreate = layoutInflater.inflate(R.layout.card_layout,parent,false);
+        }
+        else if (card_val==20){
+            view_oncreate = layoutInflater.inflate(R.layout.product_card,parent,false);
+        }
+       else {
+            view_oncreate = layoutInflater.inflate(R.layout.card_layout,parent,false);
+        }
         return new card_view_holder(view_oncreate);
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerViewAdapter.card_view_holder holder, int position) {
 
-        holder.cat_btn.setText(title.get(position));
-        holder.cat_img_v.setImageResource(images.get(position));
+        if (card_val==10){
+            holder.cat_btn.setText(title.get(position));
+            holder.cat_img_v.setImageResource(images.get(position));
+        }
+        else if (card_val==20){
+            holder.tv_product.setText(title.get(position));
+            holder.img_pro.setImageResource(images.get(position));
+        }
+        else {
+            holder.cat_btn.setText(title.get(position));
+            holder.cat_img_v.setImageResource(images.get(position));
+        }
+
 
     }
 
@@ -53,10 +77,20 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         ImageView cat_img_v;
         Button cat_btn;
 
+        TextView tv_product;
+        ImageView img_pro;
+
         public card_view_holder(@NonNull  View itemView) {
             super(itemView);
-            cat_btn=(Button)itemView.findViewById(R.id.btn_cat);
-            cat_img_v=(ImageView)itemView.findViewById(R.id.image_view_cat);
+
+            if (card_val==10){
+                cat_btn=(Button)itemView.findViewById(R.id.btn_cat);
+                cat_img_v=(ImageView)itemView.findViewById(R.id.image_view_cat);
+            }
+            if (card_val==20){
+                img_pro=(ImageView)itemView.findViewById(R.id.image_view_product);
+                tv_product=(TextView)itemView.findViewById(R.id.text_view_product);
+            }
         }
     }
     
